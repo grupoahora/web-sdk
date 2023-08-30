67d84adc469eb10e9b0dabc90098fdbb8c5f5ab5
Andres Patiño <aantiinoo@gmail.com>
2023-08-30 15:35:33 -0500

    add enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: '/',
+    baseUrl: 'https://api.verifik.co/',
     projectId: '6332941ccde4f719d9c00f9e',
-    redirectUrl: 'https://demo.verifik.co/',
-    biometricUrl: '/'
+    redirectUrl: 'https://api.verifik.co/',
+    biometricUrl: 'https://api.verifik.co/'
 };
